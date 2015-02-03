1b69037df5565fef9eacab86e350acd6c4ce65c6
rmqlife <rmqlife@gmail.com>
2015-02-04 00:58:38 +0800

    edit by client

--- play_file.py
+++ play_file.py
@@ -6,15 +6,15 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 def play_single(file_path):
-	print i.encode('utf8')
-	os.system("omxplayer "+os.path.join(target_dir,i.encode('utf8')))
+	print file_path.encode('utf8')
+	os.system("omxplayer "+file_path.encode('utf8'))
 
 
 def play_dir(target_dir):
 	print target_dir
 	for i in os.listdir(target_dir):
 		if i.lower().endswith('mp3'):
-			play_single(i)
+			play_single(os.path.join(target_dir,i))
 
 if __name__=="__main__":
 	play_dir(sys.argv[1])
