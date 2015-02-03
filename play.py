4d489dccccb107cbf7356203ea03226effb499af
rmqlife <rmqlife@gmail.com>
2015-02-04 00:03:53 +0800

    add keyboard interrupt

--- play.py
+++ play.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 import sys
 # coding=utf8
@@ -9,6 +10,14 @@ target_dir=sys.argv[1]
 
 print target_dir
 for i in os.listdir(target_dir):
-	print i.encode('utf8')
-	os.system("omxplayer "+os.path.join(target_dir,i.encode('utf8')))
+	if i.lower().endswith('mp3'):
+		print i.encode('utf8')
+		try:
+			os.system("omxplayer "+os.path.join(target_dir,i.encode('utf8')))
+		except KeyboardInterrupt:
+			print 'cancel play'
+			try:
+				sys.exit(0)
+			except SystemExit:
+				os._exit(0)
 
