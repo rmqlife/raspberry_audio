454f3b260e01c5cb17cea695fba16ce5ce92c845
rmqlife <rmqlife@gmail.com>
2015-02-17 22:21:05 +0800

    add lsdir method

--- rmp.py
+++ rmp.py
@@ -1,21 +1,22 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask,render_template,url_for,request,redirect
 # -*- coding: utf-8 -*-
-import sys
-import os
+import sys,os
 reload(sys)
 sys.setdefaultencoding('utf-8')
 LIB_FOLDER="./Music"
 UPLOAD_FOLDER="upload"
+ALLOWED_EXTENSIONS=set(['mp3','aac','wav'])
 app = Flask(__name__)
 app.config['LIB_FOLDER'] = LIB_FOLDER
 app.config['UPLOAD_FOLDER'] = os.path.join(LIB_FOLDER,UPLOAD_FOLDER)
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
 	os.mkdir(app.config['UPLOAD_FOLDER'])
-
 with app.test_request_context('/hello'):
 	assert request.path == '/hello'
+
+def allowed_file(filename):
+	return '.' in filename and \
+		filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/upload/',methods=['GET','POST'])
 def upload_file():
@@ -23,16 +24,23 @@ def upload_file():
 		f =  request.files['file']
 		f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))	
 	return render_template("upload.html")
-		
+
 @app.route('/hello/')
 @app.route('/hello/<name>')
-def hello(name=None):
+def say_hello(name=None):
 	return render_template("hello.html",name=name)
+
+#@app.errorhandler(404)
+#def page_not_found(error):
+#	pass
 
 @app.route('/')
 def index():
 	return render_template("index.html")
 
+@app.route('/aloha/')
+def aloha():
+	return redirect(url_for("say_hello",name="Hawaii"))
 
 @app.route('/kill/')
 def kill():
@@ -40,7 +48,7 @@ def kill():
 	kill_all()
 	return list_file()
 	
-@app.route('/pl/<path:file_path>')
+@app.route('/play/<path:file_path>')
 def play_single(file_path):
 	from play_file import play_single 
 	from flask import render_template
@@ -48,17 +56,31 @@ def play_single(file_path):
 	return render_template("play.html",pid=p.pid,file_path=file_path)
 
 @app.route('/ls/')
-def list_file():
-	filepath_list=list()
-	for root,dirs,files in os.walk(app.config['LIB_FOLDER']):
+@app.route('/ls/<path:subdir>')
+def list_file(subdir=app.config['LIB_FOLDER']):
+	if subdir==None or not os.path.exists(subdir):
+		return redirect(url_for('list_file'))
+	output=render_template("ls.html")
+	for root,dirs,files in os.walk(subdir):
 		for filepath in files:
-			if filepath.endswith('.mp3'):
-				print os.path.join(root,filepath)
-				filepath_list.append(os.path.join(root,filepath))
-	output=""
-	for path in filepath_list:
-		link="/pl/%s"% (path)
-		output = output + '''<p><a href="%s">%s</a></p>'''%(link , path)
+			if allowed_file(filepath):
+				full_path=os.path.join(root,filepath)
+				link=url_for('play_single',file_path=full_path)
+				output = output + '''<p><a href="%s">%s</a></p>'''%(link , filepath)
+	return output
+
+@app.route('/lsdir/')
+@app.route('/lsdir/<path:subdir>')
+def list_dir(subdir=app.config['LIB_FOLDER']):
+	if subdir==None or not os.path.exists(subdir):
+		return redirect(url_for('list_dir'))
+	output=render_template("ls.html")
+	for d in os.listdir(subdir):
+		full_path=os.path.join(app.config['LIB_FOLDER'],d)
+		if os.path.isdir(full_path):
+			print d
+			link=url_for('list_file',subdir=full_path)
+			output = output + '''<p><a href="%s">%s</a></p>'''%(link , full_path)
 	return output
 
 @app.route('/user/<username>')
